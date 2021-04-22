a88e4ed17cd2b92a68305023df4b68d65dbf330c
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-04-22 19:19:57 +0200

    wip

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,29 @@
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Web Audio Experiments</title>
+				<meta name="title" content="Web Audio Experiments" />
+				<meta name="description" content="Making a simple synth with nothing but white noise and Web APIs to learn how sound works." />
+
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://web-audio-experiments.m1guelpf.me/" />
+				<meta property="og:title" content="Web Audio Experiments" />
+				<meta property="og:description" content="Making a simple synth with nothing but white noise and Web APIs to learn how sound works." />
+				<meta property="og:image" content="https://web-audio-experiments.m1guelpf.me/card.jpg" />
+
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://web-audio-experiments.m1guelpf.me/" />
+				<meta property="twitter:title" content="Web Audio Experiments" />
+				<meta property="twitter:description" content="Making a simple synth with nothing but white noise and Web APIs to learn how sound works." />
+				<meta property="twitter:image" content="https://web-audio-experiments.m1guelpf.me/card.jpg" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
